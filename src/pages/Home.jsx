fb8c0e1834687ff03f70488109d62ad6a5df7fcc
cannell001 <cannell001@gannon.com>
2024-06-27 13:25:34 -0400

    Fetch smoothies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,46 @@
 import supabase from '../config/supabaseClient'
+import { useEffect, useState } from 'react'
 
-console.log(supabase)
+// components
+import SmoothieCard from '../components/SmoothieCard'
 
 const Home = () => {
-    console.log('Supabase URL:', supabaseUrl)
-    console.log('Supabase Key:', supabaseKey)
+    const [fetchError, setFetchError] = useState(null)
+    const [smoothies, setSmoothies] = useState(null)
+
+    useEffect(() => {
+        const fetchSmoothies = async () => {
+            const { data, error } = await supabase.from('smoothies').select()
+
+            if (error) {
+                setFetchError('Could not fetch the smoothies')
+                setSmoothies(null)
+            }
+            if (data) {
+                setSmoothies(data)
+                setFetchError(null)
+            }
+        }
+
+        fetchSmoothies()
+    }, [])
 
     return (
         <div className='page home'>
-            <h2>Home</h2>
+            {fetchError && <p>{fetchError}</p>}
+            {smoothies && (
+                <div className='smoothies'>
+                    {/* order-by buttons */}
+                    <div className='smoothie-grid'>
+                        {smoothies.map((smoothie) => (
+                            <SmoothieCard
+                                key={smoothie.id}
+                                smoothie={smoothie}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
