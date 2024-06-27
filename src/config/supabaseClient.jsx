a255ff24cfc12d4f5e7f54d4b142b5892b32de74
cannell001 <cannell001@gannon.com>
2024-06-27 13:51:47 -0400

    Insert Data Form

--- src/config/supabaseClient.jsx
+++ src/config/supabaseClient.jsx
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log(supabaseUrl)
-console.log(supabaseKey)
+// console.log(supabaseUrl)
+// console.log(supabaseKey)
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
