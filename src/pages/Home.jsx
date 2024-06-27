55efc224378e9f51856021bd254b59218a387399
cannell001 <cannell001@gannon.com>
2024-06-26 21:08:10 -0400

    connection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import supabase from '../config/supabaseClient'
+
+console.log(supabase)
+
 const Home = () => {
-  return (
-    <div className="page home">
-      <h2>Home</h2>
-    </div>
-  )
+    console.log('Supabase URL:', supabaseUrl)
+    console.log('Supabase Key:', supabaseKey)
+
+    return (
+        <div className='page home'>
+            <h2>Home</h2>
+        </div>
+    )
 }
 
 export default Home
